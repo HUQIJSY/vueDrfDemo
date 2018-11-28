19e821384ae51404048fa0b5a0996a5a4891abb1
liuzhichao <2663412229@qq.com>
2018-11-28 16:05:46 +0800

    Changed# change debug

--- vueDrfDemo/settings.py
+++ vueDrfDemo/settings.py
@@ -124,7 +124,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.path.dirname(BASE_DIR, 'static', )
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 
 STATIC_URL = '/static/'
 
